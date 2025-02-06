b43522f48a2aa490b1d5345a511f4ccd545917c4
kambaikariuki <kariukikamba@gmail.com>
2025-02-06 13:34:52 +0300

    organized sms into different categories

--- backend/clean.py
+++ backend/clean.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-
+import re
 # Parse XML file into Element tree
 
 def get_body():
@@ -14,21 +14,82 @@ def get_body():
             f.write(str(item) + "\n")
 
 
+def organize():
 
+    categories = {
+    'incoming' : [],
+    'outgoing' : [],
+    'code_payments' : [],
+    'bank_deposit' : [],
+    'withdrawals' : [],
+    'bank_transfer' : [],
+    'cash_power' : [],
+    'other' : [],
+    'airtime_bill' : [],
+    'bundle_purchase': [],
+    'third_party' : [],
+    'failed' : [],
+    'reversals' : []
+    }
+    code_pattern = r"([A-Z][a-z]+ [A-Z][a-z]+ \d{5})"
+    logged = 0
+    unlogged = 0
 
-get_body()
+    with open('backend/sms.txt', 'r') as f:
+        for line in f:
+            if "failed" in line:
+                categories['failed'].append(line)
+                logged += 1
+            else:
+                if "received" in line:
+                    categories['incoming'].append(line)
+                    logged += 1
+                elif re.search(code_pattern, line):
+                    categories['code_payments'].append(line)
+                    logged += 1
+                elif "transferred" in line:
+                    categories['outgoing'].append(line)
+                    logged += 1
+                elif "bank deposit" in line:
+                    categories['bank_deposit'].append(line)
+                    logged += 1
+                elif "Airtime" in line:
+                    categories['airtime_bill'].append(line)
+                    logged += 1
+                elif "Bundles and Packs" in line:
+                    categories['bundle_purchase'].append(line)
+                    logged += 1
+                elif "Y'ello" in line:
+                    if "Data" in line:
+                        categories['bundle_purchase'].append(line)
+                    else:
+                        categories['third_party'].append(line)
+                    logged += 1
+                elif "withdrawn" in line:
+                    categories['withdrawals'].append(line)
+                    logged += 1
+                elif "Cash Power" in line:
+                    categories['cash_power'].append(line)
+                    logged += 1
+                elif "Yello!" in line:
+                    if "Mins" in line:
+                        categories['airtime_bill'].append(line)
+                    else:
+                        categories['bundle_purchase'].append(line)
+                    logged += 1
+                elif "revers" in line:
+                    categories['reversals'].append(line)
+                    logged += 1
+                else:
+                    categories['other'].append(line)
+                    unlogged += 1
+ 
+    for category, messages in categories.items():
+        file = f'{category}.txt'
+        with open(file, "w") as f:
+            for message in messages:
+                f.write(message)
 
+    print(f"Successfully logged {logged} messages, {unlogged} others saved in other.txt")
 
-
-
-# new_root = ET.Element('clean_data')
-
-# tags = ["body","readable_date"]
-
-# for tag in tags:
-#     for elem in root.findall(f".//{tag}"):
-#         new_root.append(elem)
-
-# new_tree = ET.ElementTree(new_root)
-# new_tree.write('clean_data.xml')
 
