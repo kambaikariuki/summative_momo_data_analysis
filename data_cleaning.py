4d13c5d01c9df0a7906becb3d073155dca713ca7
Otani-ibe <o.ibe@alustudent.com>
2025-02-12 10:47:54 +0200

    data cleaning

--- data_cleaning.py
+++ data_cleaning.py
@@ -26,4 +26,4 @@ for match in pattern.finditer(incoming_data_text):
 with open("transactions.json", "w") as json_file:
     json.dump(transactions, json_file, indent=4)
 
-print("Data has been successfully saved to transactions.json")
+print("Data has been successfully saved to transactions.json!")
