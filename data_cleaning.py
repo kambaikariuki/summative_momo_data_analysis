b32fa42f473348321f79a7b7542906ceb63318b7
Otani-ibe <o.ibe@alustudent.com>
2025-02-12 13:30:44 +0200

    Data cleaning

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,7 +12,7 @@ with open("Incoming.txt", "r") as file:
 
 transactions = []
 
-# Extracting data using the regex pattern
+# Extracting data using the regular expression pattern
 for match in pattern.finditer(incoming_data_text):
     amount, sender_details, date_time, new_balance, transaction_id = match.groups()
     transactions.append({
@@ -22,7 +22,7 @@ for match in pattern.finditer(incoming_data_text):
         "new_balance": int(new_balance),
         "transaction_id": transaction_id})
 
-# Saving the extracted data to a JSON file
+# Saving the extracted data to a JSON file: transactions.json
 with open("transactions.json", "w") as json_file:
     json.dump(transactions, json_file, indent=4)
 
